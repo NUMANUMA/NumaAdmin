393c715f4b7fff21bfde18c6d53612ba20441093
NUMANUMA <nazottewho@yahoo.co.jp>
2013-10-14 20:55:05 +0900

    NumaAdmin

---
+++ src/com/github/NUMANUMA/NumaAdmin/Event.java
@@ -0,0 +1,12 @@
+package com.github.NUMANUMA.NumaAdmin;
+
+import org.bukkit.event.Listener;
+
+public class Event implements Listener {
+
+    public Event(NumaAdmin plugin){
+    }
+
+    //イベントたち
+
+}
---
+++ src/com/github/NUMANUMA/NumaAdmin/NumaAdmin.java
@@ -0,0 +1,23 @@
+package com.github.NUMANUMA.NumaAdmin;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NumaAdmin extends JavaPlugin{
+    Logger log;
+    public Event eve = new Event(this);
+
+    @Override
+    public void onEnable() {
+        log = this.getLogger();
+        log.info("プラグインが有効になりました。");
+
+        getServer().getPluginManager().registerEvents(eve, this);
+    }
+
+    @Override
+    public void onDisable() {
+        log.info("プラグインが無効になりました。");
+    }
+}
