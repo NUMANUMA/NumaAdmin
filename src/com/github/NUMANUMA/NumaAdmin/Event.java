ad68f12e391653f1172e906a793dcf7757705304
NUMANUMA <nazottewho@yahoo.co.jp>
2013-10-14 22:54:19 +0900

    ユーザーを指定してゲームモードを変更する[失敗]
    args[1]の呪い！

--- src/com/github/NUMANUMA/NumaAdmin/Event.java
+++ src/com/github/NUMANUMA/NumaAdmin/Event.java
@@ -8,9 +8,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
-public class Event implements Listener,CommandExecutor {
+public class Event implements Listener, CommandExecutor {
 
-    public Event(NumaAdmin plugin){
+    public Event(NumaAdmin plugin) {
     }
 
     //イベントたち
@@ -18,37 +18,60 @@ public class Event implements Listener,CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         Player player = null;
-        if(sender instanceof Player) {
+        if (sender instanceof Player) {
             player = (Player) sender;
         }
 
         //gmコマンド
-        if(cmd.getName().equalsIgnoreCase("gm")){
-            if(player != null) {
-              //選択
-                if(args.length == 0) {
+        if (cmd.getName().equalsIgnoreCase("gm")) {
+            if (player != null) {
+                if (args.length == 0) {
                     player.sendMessage(ChatColor.RED + "数値が正しくありません。");
 
                     return false;
-                } else if(args[0].equalsIgnoreCase("0")) {
-                    player.sendMessage(ChatColor.AQUA + "サバイバルモードに変更しました。");
-                    player.setGameMode(GameMode.SURVIVAL);
+                } else if (args.length == 1) {
+                    //ゲームモードの選択選択
+                    if (args[0].equalsIgnoreCase("0")) {
 
-                    return true;
-                } else if(args[0].equalsIgnoreCase("1")) {
-                    player.sendMessage(ChatColor.AQUA + "クリエイティブモードに変更しました。");
-                    player.setGameMode(GameMode.CREATIVE);
+                        player.sendMessage(ChatColor.AQUA + "サバイバルモードに変更しました。");
+                        player.setGameMode(GameMode.SURVIVAL);
 
-                    return true;
-                } else if(args[0].equalsIgnoreCase("2")) {
-                    player.sendMessage(ChatColor.AQUA + "アドベンチャーモードに変更しました。");
-                    player.setGameMode(GameMode.ADVENTURE);
+                        return true;
+                    } else if (args[0].equalsIgnoreCase("1")) {
 
-                    return true;
-                } else {
-                    player.sendMessage(ChatColor.RED + "数値が正しくありません。");
+                        player.sendMessage(ChatColor.AQUA + "クリエイティブモードに変更しました。");
+                        player.setGameMode(GameMode.CREATIVE);
 
-                    return false;
+                        return true;
+                    } else if (args[0].equalsIgnoreCase("2")) {
+                        /*
+                         * 動かない！！！！！！
+                         * なんで！！！！
+                        if(!args[1].isEmpty()) {
+                            Player other = (Bukkit.getServer().getPlayer(args[1]));
+                            //対象の選択
+                            if(other == null) {
+                                player.sendMessage(ChatColor.RED + args[1] + "は、オフラインです！");
+                                return false;
+                            } else {
+                                //ここに実行したいことをかく
+                                other.sendMessage(ChatColor.AQUA + "アドベンチャーモードに変更しました。");
+                                other.setGameMode(GameMode.ADVENTURE);
+                                player.sendMessage(ChatColor.AQUA + args[1] + "を、アドベンチャーモードに変更しました。");
+
+                                return true;
+                            }
+                        }
+                        */
+                        player.sendMessage(ChatColor.AQUA + "アドベンチャーモードに変更しました。");
+                        player.setGameMode(GameMode.ADVENTURE);
+
+                        return true;
+                    } else {
+                        player.sendMessage(ChatColor.RED + "数値が正しくありません。");
+
+                        return false;
+                    }
                 }
             } else {
                 sender.sendMessage("このコマンドはプレイヤー専用です。");
