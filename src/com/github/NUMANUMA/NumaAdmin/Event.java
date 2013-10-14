4a21d14e3a3a3a129f8334972851397ffad1f887
NUMANUMA <nazottewho@yahoo.co.jp>
2013-10-14 21:23:01 +0900

    ゲームモードの変更
    テストしてないから動くかわからん

--- src/com/github/NUMANUMA/NumaAdmin/Event.java
+++ src/com/github/NUMANUMA/NumaAdmin/Event.java
@@ -1,5 +1,10 @@
 package com.github.NUMANUMA.NumaAdmin;
 
+import org.bukkit.Color;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 public class Event implements Listener {
@@ -8,5 +13,39 @@ public class Event implements Listener {
     }
 
     //イベントたち
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+        Player player = null;
+        if(sender instanceof Player) {
+            player = (Player) sender;
+        }
+
+        //gmコマンド
+        if(cmd.getName().equalsIgnoreCase("gm")){
+            //サバイバルを選択
+            if(args[0] == "0") {
+                player.sendMessage(Color.AQUA + "サバイバルモードに変更しました。");
+                player.setGameMode(GameMode.SURVIVAL);
+
+                return true;
+            } else if(args[0] == "1") {
+                player.sendMessage(Color.AQUA + "クリエイティブモードに変更しました。");
+                player.setGameMode(GameMode.CREATIVE);
+
+                return true;
+            } else if(args[0] == "2") {
+                player.sendMessage(Color.AQUA + "アドベンチャーモードに変更しました。");
+                player.setGameMode(GameMode.ADVENTURE);
+
+                return true;
+            } else {
+                player.sendMessage(Color.RED + "[0=サバイバル,1=クリエイティブ,2=アドベンチャー]");
+
+                return true;
+            }
+
+        }
+
+        return false;
+    }
 }
